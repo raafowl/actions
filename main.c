d332f6f03707fb46fcf82147d6d255512f8d4902
raafowl <83341324+raafowl@users.noreply.github.com>
2021-11-12 10:55:11 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <inttypes.h>
 
 int main()
 {
     printf("Hello World.\n");
+    printf("Platform: %s.\n", SIZE_MAX == UINT64_MAX ? "64-bit" : "32-bit");
     return 0;
 }
